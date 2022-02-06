e97075f2656b3670b8af0620ff7966a3e6839636
Stanislav Barsukov <min.stanis@yandex.ru>
2022-02-06 16:28:12 +0300

    "Проектная работа 6"

--- scrypt/index.js
+++ scrypt/index.js
@@ -31,7 +31,6 @@ const initialCards = [
     {
         title: "Владивосток",
         link: "https://images.unsplash.com/photo-1529111017668-af85a7fc9bab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=888&q=80"
-
     },
     {
         title: "Кисловодск",
